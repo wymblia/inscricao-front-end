6b5bf34a0618124793368edf40ab950f619345bc
wymbliathomazzoni <wymbliathomazzoni@acad.ftec.com.br>
2022-07-16 19:58:18 -0300

    ajuste na tipagem de dados e indentação

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -6,39 +6,40 @@ interface InputProps {
   classNameLabel?: string
   idInput?: string
   typeInput?: string
-  valueInput?: any
+  valueInput?: string
   classNameInput?: any
   onChange?: (valueInput: any) => void
-
   onBlur?: (valueInput: any) => void
   hidden?: boolean
-  readOnly?: boolean
   mask?: string
   defaultValue?: any
-  accept?: any
+  accept?: string
   required?: boolean
   placeholder?: string
 }
 
-export default function Input (props: InputProps) {
+export default function Input(props: InputProps) {
   return (
     <div className="flex flex-col mb-2">
-      <label className={`mb-2 font-light text-sm text-gray-700 ${props.classNameLabel}`} htmlFor={props.idInput}>
+      <label
+        className={`mb-2 font-light text-sm text-gray-700 ${props.classNameLabel}`}
+        htmlFor={props.idInput}
+      >
         {props.textLabel}
       </label>
       <InputMask
-        mask = { props.mask }
-        placeholder = { props.placeholder }
-        id = { props.idInput }
-        type = { props.typeInput }
-        value = { props.valueInput }
-        onChange = { (e => props.onChange?.(e.target.value)) }
-        onBlur = { e => props.onBlur?.(e.target.value) }
-        hidden = { props.hidden }
-        defaultValue = { props.defaultValue }
-        accept = { props.accept }
-        required = { props.required }
-        className = {`xl:w-[480px] h-10 mb-2 border border-gray-300 rounded-2xl focus:outline-none bg-gray-50 px-4 py-2 focus:bg-white text-gray-700 ${props.readOnly ? 'bg-gray-200 focus:bg-gray-200' : null} ${props.classNameInput}`}
+        mask={props.mask}
+        placeholder={props.placeholder}
+        id={props.idInput}
+        type={props.typeInput}
+        value={props.valueInput}
+        onChange={(e) => props.onChange?.(e.target.value)}
+        onBlur={props.onBlur}
+        hidden={props.hidden}
+        defaultValue={props.defaultValue}
+        accept={props.accept}
+        required={props.required}
+        className={`xl:w-[480px] h-10 mb-2 border border-gray-300 rounded-2xl focus:outline-none bg-gray-50 px-4 py-2 focus:bg-white text-gray-700}`}
       />
     </div>
   )
--- src/components/TextArea.tsx
+++ src/components/TextArea.tsx
@@ -1,26 +1,29 @@
 interface TextAreaProps {
-  name?: string
   textLabel?: string
   forLabel?: string
   classNameLabel?: string
   idTextarea?: string
-  valueTextArea?: any
+  valueTextArea?: string
   classNameInput?: string
   onChange?: (valueInput: any) => void
+  required?: boolean
 }
 
-export default function Textarea (props: TextAreaProps) {
-
+export default function Textarea(props: TextAreaProps) {
   return (
     <div className="flex flex-col mb-2 text-gray-700">
-      <label className={`mb-2 font-light text-sm ${props.classNameLabel}`} htmlFor={props.idTextarea}>
+      <label
+        className={`mb-2 font-light text-sm ${props.classNameLabel}`}
+        htmlFor={props.idTextarea}
+      >
         {props.textLabel}
       </label>
       <textarea
-        id = {props.idTextarea}
-        value = {props.valueTextArea}
-        onChange ={ e => props.onChange?.(e.target.value) }
-        className = {`h-15 mb-2 border border-gray-300 rounded-2xl focus:outline-none bg-gray-50 px-4 py-2 focus:bg-white ${props.classNameInput}`}
+        id={props.idTextarea}
+        value={props.valueTextArea}
+        onChange={(e) => props.onChange?.(e.target.value)}
+        className={`h-15 mb-2 border border-gray-300 rounded-2xl focus:outline-none bg-gray-50 px-4 py-2 focus:bg-white ${props.classNameInput}`}
+        required={props.required}
       />
     </div>
   )
