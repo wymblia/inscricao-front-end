c7b0998e8a0e3aaa506dc9cf1cd9d5c4f61a6ffd
wymbliathomazzoni <wymbliathomazzoni@acad.ftec.com.br>
2022-07-16 19:33:50 -0300

    ajuste na tipagem de dados e indentação

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,16 @@
 interface ButtonProps {
   children: any
   onClick?: () => any
-  type?: any
+  type?: "submit"
 }
 
-export default function Button ( props: ButtonProps ) {
+export default function Button(props: ButtonProps) {
   return (
-    <button type={props.type} onClick={ props.onClick } className="max-w-screen-sm bg-[#4B6BFB] shadow-md shadow-gray-300 text-white font-medium rounded-2xl p-3">
+    <button
+      type={props.type}
+      onClick={props.onClick}
+      className="max-w-screen-sm bg-[#4B6BFB] shadow-md shadow-gray-300 text-white font-medium rounded-2xl p-3"
+    >
       {props.children}
     </button>
   )
