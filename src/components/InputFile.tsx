1925cf049566844299b49e653cab3c928cc1efeb
wymbliathomazzoni <wymbliathomazzoni@acad.ftec.com.br>
2022-07-21 15:50:21 -0300

    retiradas propriedades não utilizadas

--- src/components/InputFile.tsx
+++ src/components/InputFile.tsx
@@ -2,39 +2,29 @@ import InputMask from "react-input-mask"
 
 interface InputProps {
   textLabel?: string
-  forLabel?: string
-  classNameLabel?: string
   idInput?: string
   typeInput?: string
   valueInput?: any
-  classNameInput?: any
+  accept?: string
+  required?: boolean
   onChange?: (valueInput: any) => void
   onClick?: (valueInput: any) => void
-  hidden?: boolean
-  defaultValue?: any
-  accept?: any
-  required?: boolean
-  placeholder?: string
 }
 
 export default function Input(props: InputProps) {
   return (
     <div className="flex flex-col mb-2">
-      <label className={`mb-2 font-light text-sm ${props.classNameLabel}`} htmlFor={props.idInput}>
+      <label className="mb-2 font-light text-sm" htmlFor={props.idInput}>
         {props.textLabel}
       </label>
       <InputMask
-        placeholder={props.placeholder}
         id={props.idInput}
         type={props.typeInput}
         value={props.valueInput}
         onChange={props.onChange}
         onClick={props.onClick}
-        hidden={props.hidden}
-        defaultValue={props.defaultValue}
         accept={props.accept}
         required={props.required}
-        className={`${props.classNameInput}`}
       />
     </div>
   )
