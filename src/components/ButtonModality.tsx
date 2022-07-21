ceeab2168b9404a3775af1f2de98f82f676ab60b
wymbliathomazzoni <wymbliathomazzoni@acad.ftec.com.br>
2022-07-21 17:18:16 -0300

    ajuste de margem

--- src/components/ButtonModality.tsx
+++ src/components/ButtonModality.tsx
@@ -21,7 +21,7 @@ export default function Button(props: ButtonProps) {
           onClick={props.onClick}
         />
         <label
-          className="flex py-auto flex-col mb-2 md:w-[153px] w-32 h-16 bg-white border border-blue-600 rounded-xl cursor-pointer focus:outline-none hover:bg-grey-50 focus:shadow-2xl peer-checked:ring-blue-500 peer-checked:ring-2 peer-checked:border-2 font-medium justify-center items-center text-blue-600 text-xs uppercase transition duration-400"
+          className="flex py-auto flex-col mb-3 md:w-[153px] w-32 h-16 bg-white border border-blue-600 rounded-xl cursor-pointer focus:outline-none hover:bg-grey-50 focus:shadow-2xl peer-checked:ring-blue-500 peer-checked:ring-2 peer-checked:border-2 font-medium justify-center items-center text-blue-600 text-xs uppercase transition duration-400"
           htmlFor={props.htmlFor}
         >
           {props.label}
