4787292483233286b5788e5af425873898f07720
wymbliathomazzoni <wymbliathomazzoni@acad.ftec.com.br>
2022-07-21 15:53:58 -0300

    adicionada renderização condicional se o input tiver máscara, ajuste de cores, retriadas propriedades não utilizadas

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -2,45 +2,44 @@ import InputMask from "react-input-mask"
 
 interface InputProps {
   textLabel?: string
-  forLabel?: string
-  classNameLabel?: string
   idInput?: string
   typeInput?: string
   valueInput?: string
-  classNameInput?: any
+  required?: boolean
+  existsMask?: boolean
+  mask?: string
   onChange?: (valueInput: any) => void
   onBlur?: (valueInput: any) => void
-  hidden?: boolean
-  mask?: string
-  defaultValue?: any
-  accept?: string
-  required?: boolean
-  placeholder?: string
 }
 
 export default function Input(props: InputProps) {
   return (
     <div className="flex flex-col mb-2">
-      <label
-        className={`mb-2 font-light text-sm text-gray-700 ${props.classNameLabel}`}
-        htmlFor={props.idInput}
-      >
+      <label className="mb-2 font-light text-sm text-grey-700" htmlFor={props.idInput}>
         {props.textLabel}
       </label>
-      <InputMask
-        mask={props.mask}
-        placeholder={props.placeholder}
-        id={props.idInput}
-        type={props.typeInput}
-        value={props.valueInput}
-        onChange={(e) => props.onChange?.(e.target.value)}
-        onBlur={props.onBlur}
-        hidden={props.hidden}
-        defaultValue={props.defaultValue}
-        accept={props.accept}
-        required={props.required}
-        className={`xl:w-[480px] h-10 mb-2 border border-gray-300 rounded-2xl focus:outline-none bg-gray-50 px-4 py-2 focus:bg-white text-gray-700}`}
-      />
+      {props.existsMask ? (
+        <InputMask
+          className="xl:w-[480px] h-10 mb-2 border border-grey-300 rounded-2xl focus:outline-none bg-grey-50 px-4 py-2 focus:bg-white text-grey-700 focus:border-blue-300"
+          id={props.idInput}
+          type={props.typeInput}
+          value={props.valueInput}
+          onChange={(e) => props.onChange?.(e.target.value)}
+          onBlur={props.onBlur}
+          required={props.required}
+          mask={props.mask}
+        />
+      ) : (
+        <input
+          className="xl:w-[480px] h-10 mb-2 border border-grey-300 rounded-2xl focus:outline-none bg-grey-50 px-4 py-2 focus:bg-white text-grey-700 focus:border-blue-300"
+          id={props.idInput}
+          type={props.typeInput}
+          value={props.valueInput}
+          onChange={(e) => props.onChange?.(e.target.value)}
+          onBlur={props.onBlur}
+          required={props.required}
+        />
+      )}
     </div>
   )
 }
