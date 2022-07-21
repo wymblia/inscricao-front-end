7c16e11182b28859514fb670d8ccd9b30a79f200
wymbliathomazzoni <wymbliathomazzoni@acad.ftec.com.br>
2022-07-21 17:02:35 -0300

    alteração na forma de validação do cpf,  alteração do switch, alteração de variáveis, removidos imports não utilizados, removidas e adicionadas propriedades de componentes

--- src/components/FormPersonalData.tsx
+++ src/components/FormPersonalData.tsx
@@ -1,14 +1,14 @@
 import React, { useContext, useState } from "react"
-import { useEffect } from "react"
-import Swal from "sweetalert2"
 import { RegistrationContext } from "../contexts/RegistrationContext"
 import PersonalData from "../core/PersonalData"
-import Button from "./Button"
-import ButtonBack from "./ButtonBack"
 import Input from "./Input"
 import Select from "./Select"
 import Textarea from "./TextArea"
-
+import ButtonNext from "./ButtonNext"
+import ButtonBack from "./ButtonBack"
+import Swal from "sweetalert2"
+import { cpf } from "cpf-cnpj-validator"
+import { Switch } from "@material-tailwind/react"
 interface PersonalDataProps {
   personalData: PersonalData
   personalDataChange?: (personalData: PersonalData) => void
@@ -17,8 +17,8 @@ interface PersonalDataProps {
 
 export default function FormPersonalData(props: PersonalDataProps) {
   const {
-    cpf,
-    setCpf,
+    CPF,
+    setCPF,
     birthDate,
     setBirthDate,
     switchShowDeficiency,
@@ -28,115 +28,64 @@ export default function FormPersonalData(props: PersonalDataProps) {
     gender,
     setGender
   } = useContext(RegistrationContext)
-  const [classNameInput, setClassNameInput] = useState("bg-red-900")
 
-  function handleSwitchShowDeficiency() {
-    switchShowDeficiency
-      ? (setSwitchShowDeficiency(false), setDisabilityRelief(""))
-      : setSwitchShowDeficiency(true)
-  }
-
-  function cpfValidation(cpf) {
-    cpf = cpf.replace(/[^\d]+/g, "")
-    if (cpf == "") return false
-    // Elimina CPFs invalidos conhecidos
-    if (
-      cpf.length != 11 ||
-      cpf == "00000000000" ||
-      cpf == "11111111111" ||
-      cpf == "22222222222" ||
-      cpf == "33333333333" ||
-      cpf == "44444444444" ||
-      cpf == "55555555555" ||
-      cpf == "66666666666" ||
-      cpf == "77777777777" ||
-      cpf == "88888888888" ||
-      cpf == "99999999999"
-    ) {
-      return false
-    }
-    // Valida 1o digito
-    let add = 0
-    for (let i = 0; i < 9; i++) {
-      add += parseInt(cpf.charAt(i)) * (10 - i)
-    }
-    let rev = 11 - (add % 11)
-    if (rev == 10 || rev == 11) {
-      rev = 0
-    }
-    if (rev != parseInt(cpf.charAt(9))) {
-      return false
-    }
-    // Valida 2o digito
-    add = 0
-    for (let i = 0; i < 10; i++) {
-      add += parseInt(cpf.charAt(i)) * (11 - i)
-    }
-    rev = 11 - (add % 11)
-    if (rev == 10 || rev == 11) {
-      rev = 0
-    }
-    if (rev != parseInt(cpf.charAt(10))) {
-      return false
-    }
-    return true
-  }
-
-  function handleCpf() {
-    if (cpf.includes("_")) {
+  function handleIncompleteAndInvalidCpf() {
+    if (CPF.includes("_")) {
       Swal.fire({
         title: "CPF incompleto",
         text: "Digite o CPF completo!",
         confirmButtonText: "Ok",
         icon: "warning"
       })
+      return true
     } else {
-      if (!cpfValidation(cpf)) {
+      if (!cpf.isValid(CPF)) {
         Swal.fire({
           title: "CPF inválido",
           text: "Digite seu CPF!",
           confirmButtonText: "Ok",
           icon: "warning"
         })
-        setCpf("")
+        setCPF("")
+        return true
       }
     }
   }
 
   function FormSubmit(e: any) {
     e.preventDefault()
-    props.personalDataChange?.(new PersonalData(cpf, new Date(birthDate), disabilityRelief))
+    handleIncompleteAndInvalidCpf()
+      ? null
+      : props.personalDataChange?.(new PersonalData(CPF, new Date(birthDate), disabilityRelief))
   }
 
   return (
     <div>
       <form onSubmit={FormSubmit}>
         <Input
-          mask="999.999.999-99"
           textLabel="CPF"
-          defaultValue={cpf || ""}
-          valueInput={cpf}
-          typeInput="text"
           idInput="cpf"
-          onChange={setCpf}
-          onBlur={handleCpf}
+          typeInput="text"
+          valueInput={CPF}
+          onChange={setCPF}
+          existsMask={true}
+          mask={"999.999.999-99"}
           required
         />
 
         <Input
-          classNameInput={"text-gray-700"}
-          classNameLabel={"text-gray-700"}
           textLabel="Data de Nascimento"
-          typeInput="date"
           idInput="birthDate"
-          defaultValue={birthDate}
+          typeInput="date"
+          valueInput={String(birthDate)}
           onChange={setBirthDate}
           required
         />
 
         <Select
           textLabel="Gênero"
-          defaultValue={gender}
+          idSelect="gender"
+          value={gender}
           onChange={(e) => setGender(e.target.value)}
           required
         >
@@ -147,24 +96,20 @@ export default function FormPersonalData(props: PersonalDataProps) {
           <option value="F">Feminino</option>
         </Select>
 
-        <div className="flex justify-end -mb-4z text-sm">
-          <div className="form-check form-switch">
-            <input
+        <div className="flex justify-start text-sm mb-2 ml-2">
+          <div className="flex w-max gap-4">
+            <Switch
+              id="socialName"
+              color="blue"
+              label="Sou portador de necessidades especiais"
               defaultChecked={switchShowDeficiency}
-              className="form-check-input appearance-none w-9 -ml-10 rounded-full float-left h-5 align-top bg-no-repeat bg-contain bg-gray-300 focus:outline-none cursor-pointer shadow-sm"
-              type="checkbox"
-              role="switch"
-              id="flexSwitchCheckDefault"
-              onClick={handleSwitchShowDeficiency}
+              onChange={() => (
+                setSwitchShowDeficiency(!switchShowDeficiency), setDisabilityRelief("")
+              )}
             />
-            <label
-              className="form-check-label inline-block text-gray-800 mb-3"
-              htmlFor="flexSwitchCheckDefault"
-            >
-              Sou portador de necessidades especiais
-            </label>
           </div>
         </div>
+
         <div hidden={!switchShowDeficiency}>
           <Textarea
             textLabel="Necessito da seguinte providência:"
@@ -175,8 +120,9 @@ export default function FormPersonalData(props: PersonalDataProps) {
         </div>
 
         <div className="flex flex-col mt-12">
-          <Button type="submit">Próximo</Button>
+          <ButtonNext type="submit">Próximo</ButtonNext>
         </div>
+
         <div className="flex flex-col mt-2">
           <ButtonBack onClick={() => props.backPage()}>Voltar</ButtonBack>
         </div>
